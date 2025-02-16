c1483626d3be3b57be407ca58940b06251585d8b
wellers <paul_welbourne@yahoo.co.uk>
2025-02-16 18:17:55 +0000

    Enhanced features on server.

--- server/go.mod
+++ server/go.mod
@@ -1,2 +1,5 @@
 module mymodule
+
 go 1.23
+
+require github.com/gorilla/mux v1.8.1
--- server/main.go
+++ server/main.go
@@ -1,27 +1,65 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+type User struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+	Age       int    `json:"age"`
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-	})
+	r := mux.NewRouter()
 
-	server := &http.Server{Addr: ":80"}
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(w, "Hello, World!")
+	}).Methods("GET")
 
-	listener, err := net.Listen("tcp", server.Addr)
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	}).Methods("GET")
+
+	r.HandleFunc("/decode", func(w http.ResponseWriter, r *http.Request) {
+		var user User
+		json.NewDecoder(r.Body).Decode(&user)
+
+		fmt.Fprintf(w, "%s %s is %d years old!", user.Firstname, user.Lastname, user.Age)
+	}).Methods("POST")
+
+	r.HandleFunc("/encode", func(w http.ResponseWriter, r *http.Request) {
+		peter := User{
+			Firstname: "John",
+			Lastname:  "Doe",
+			Age:       25,
+		}
+
+		json.NewEncoder(w).Encode(peter)
+	}).Methods("GET")
+
+	ListenAndServe(r)
+}
+
+func ListenAndServe(r *mux.Router) {
+	listener, err := net.Listen("tcp", ":80")
 	if err != nil {
 		fmt.Println("Error creating listener:", err)
 		return
 	}
 
-	fmt.Println("Listening on port 80")
+	fmt.Println("Listening on port 80...")
 
-	err = server.Serve(listener)
+	err = http.Serve(listener, r)
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 	}
